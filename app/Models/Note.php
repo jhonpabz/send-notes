d424b841af4beff5dbc8a4a112fe39c703c1495f
jhonpabz <jhncsrpbl@gmail.com>
2025-01-28 17:43:45 +0800

    update: table and models

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -9,4 +9,26 @@ use Illuminate\Database\Eloquent\Model;
 class Note extends Model
 {
     use HasFactory, HasUuids;
+
+    // protected $fillable = [
+    //     "title",
+    //     "body",
+    //     "sent_date",
+    //     "is_published",
+    //     "heart_count",
+    //     "timestamps",
+    //     "create_at",
+    //     "updated_at",
+    //     "deleted_at"
+    // ];
+
+    protected $guarded = [
+        "id",
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
