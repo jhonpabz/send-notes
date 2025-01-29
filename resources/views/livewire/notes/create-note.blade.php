84a52106625234bad43c8b4d8138d3718e7b503f
jhonpabz <jhncsrpbl@gmail.com>
2025-01-29 20:46:04 +0800

    add: validation

--- resources/views/livewire/notes/create-note.blade.php
+++ resources/views/livewire/notes/create-note.blade.php
@@ -10,16 +10,24 @@ new class extends Component {
 
     public function submit()
     {
+        $validate = $this->validate([
+            'noteTitle' => ['required', 'string', 'min:5'],
+            'noteBody' => ['required', 'string', 'min:20'],
+            'noteRecipient' => ['required', 'email'],
+            'noteSentDate' => ['required', 'date'],
+        ]);
         dd($this->noteTitle, $this->noteBody, $this->noteRecipient, $this->noteSentDate);
     }
 }; ?>
 
 <div>
-    <form wire:submit='submit'>
-        <x-input wire:model="noteTitle" label="Title" />
-        <x-textarea wire:model="noteBody" label="Body" />
-        <x-input wire:model="noteRecipient" label="Recipient" />
+    <form wire:submit='submit' class="space-y-4">
+        <x-input wire:model="noteTitle" label="Note Title" />
+        <x-textarea wire:model="noteBody" label="Your Note" placeholder="Share all your thoughts.." />
+        <x-input wire:model="noteRecipient" label="Recipient" placeholder="email@gmail.com" type="email" />
         <x-input wire:model="noteSentDate" type="date" label="Sent Date" />
-        <x-button wire:click='submit'>Submit</x-button>
+        <div class="pt-4">
+            <x-button wire:click='submit' right-icon="calendar" spinner>Submit </x-button>
+        </div>
     </form>
 </div>
