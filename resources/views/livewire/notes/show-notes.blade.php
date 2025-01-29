1406fbf3c14b71874bc75e16f5e44c4634c36dba
jhonpabz <jhncsrpbl@gmail.com>
2025-01-29 17:05:05 +0800

    add: card and display title and sent date

--- resources/views/livewire/notes/show-notes.blade.php
+++ resources/views/livewire/notes/show-notes.blade.php
@@ -12,7 +12,16 @@ new class extends Component {
 }; ?>
 
 <div>
-    @foreach ($notes as $note)
-        {{ $note ->title }}
-    @endforeach
+    <div class="space-y-2">
+        @foreach ($notes as $note)
+            <x-card wire:key="{{ $note->id }}">
+                <div class="flex justify-between">
+                    <a href="#" class="text-xl font-bold hover:underline hover:text-blue-500">{{ $note ->title}}</a>
+                    <div class="text-xs text-gray-500">
+                    {{ \Carbon\Carbon::parse($note->sent_date)->format('M-d-Y')}}
+                    </div>
+                </div>
+            </x-card>
+        @endforeach
+    </div>
 </div>
