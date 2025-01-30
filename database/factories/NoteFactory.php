68f39e84202081e8ed036972945a993b3b09e119
jhonpabz <jhncsrpbl@gmail.com>
2025-01-30 17:32:18 +0800

    define note factory

--- database/factories/NoteFactory.php
+++ database/factories/NoteFactory.php
@@ -17,7 +17,14 @@ class NoteFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'id' => $this->faker->uuid,
+            'user_id' => \App\Models\User::factory(),
+            'title' => $this->faker->sentence,
+            'body' => $this->faker->paragraph(3, true),
+            'recipient' => $this->faker->email,
+            'sent_date' => $this->faker->dateTimeBetween('now', '+10 days', ),
+            'is_published' => true,
+            'heart_count' => $this->faker->numberBetween(0,100),
         ];
     }
 }
