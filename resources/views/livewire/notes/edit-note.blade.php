226ddee2f1d65a50e61548a8a1dd3e598633ea8c
jhonpabz <jhncsrpbl@gmail.com>
2025-01-30 14:24:42 +0800

    display edit note data

--- resources/views/livewire/notes/edit-note.blade.php
+++ resources/views/livewire/notes/edit-note.blade.php
@@ -2,13 +2,20 @@
 
 use Livewire\Volt\Component;
 use Livewire\Attributes\Layout;
+use App\Models\Note;
 
 new #[Layout('layouts.app')] class extends Component {
+    public Note $note;
 
-
-    //
+    public function mount(Note $note)
+    {
+        $this->fill($note);
+    }
 }; ?>
 
 <div>
-    Edit note page
+    <h1>{{ $note->id}}</h1>
+    <h1>{{ $note->recipient}}</h1>
+    <h1>{{ $note->title}}</h1>
+    <h1>{{ $note->body}}</h1>
 </div>
