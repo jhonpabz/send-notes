8ee4d3fd10082c558387b624934f37f7c93d657d
jhonpabz <jhncsrpbl@gmail.com>
2025-01-30 14:36:12 +0800

    add: autorization [Policy]

--- app/Policies/NotePolicy.php
+++ app/Policies/NotePolicy.php
@@ -37,7 +37,7 @@ class NotePolicy
      */
     public function update(User $user, Note $note): bool
     {
-        //
+        return $user->id === $note->user_id;
     }
 
     /**
@@ -45,7 +45,7 @@ class NotePolicy
      */
     public function delete(User $user, Note $note): bool
     {
-        return $user->id = $note->user_id;
+        return $user->id === $note->user_id;
     }
 
     /**
--- resources/views/livewire/notes/edit-note.blade.php
+++ resources/views/livewire/notes/edit-note.blade.php
@@ -9,6 +9,7 @@ new #[Layout('layouts.app')] class extends Component {
 
     public function mount(Note $note)
     {
+        $this->authorize('update',$note);
         $this->fill($note);
     }
 }; ?>
