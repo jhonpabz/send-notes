e3f99f9a39617e84a93c9063ee7616b0858f7673
jhonpabz <jhncsrpbl@gmail.com>
2025-01-30 17:42:38 +0800

    create fake data
    php artisan db:seed

--- database/seeders/NoteSeeder.php
+++ database/seeders/NoteSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\Note;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,10 @@ class NoteSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::all()->each(function (User $user) {
+            Note::factory()->count(10)->create([
+                'user_id' => $user->id,
+            ]);
+        });
     }
 }
